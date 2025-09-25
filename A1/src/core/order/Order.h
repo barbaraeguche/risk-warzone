8b49519b66d7d9070ef4f069595fa1896b4ee7bc
Yan Pilon <y.pilon9@gmail.com>
2025-09-25 14:20:11 -0400

    class definitions for order, order subclasses and orderlist

---
+++ A1/src/core/order/Order.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+class Order {
+public:
+  virtual bool validate() = 0;
+  virtual void execute() = 0;
+  virtual Order& operator=(const Order& other) = 0;
+  virtual std::ostream& print(std::ostream& os) const = 0;
+  virtual ~Order() = default;
+};
+
+class OrderDeploy : public Order {
+public:
+  OrderDeploy();
+  OrderDeploy(const OrderDeploy& other);
+  bool validate() override;
+  void execute() override;
+  OrderDeploy& operator=(const OrderDeploy& other);
+  std::ostream& print(std::ostream& os) const override;
+};
+
+class OrderAdvance : public Order {
+public:
+  OrderAdvance();
+  OrderAdvance(const OrderAdvance& other);
+  bool validate() override;
+  void execute() override;
+  OrderAdvance& operator=(const OrderAdvance& other);
+  std::ostream& print(std::ostream& os) const override;
+};
+
+class OrderBomb : public Order {
+public:
+  OrderBomb();
+  OrderBomb(const OrderBomb& other);
+  bool validate() override;
+  void execute() override;
+  OrderBomb& operator=(const OrderBomb& other);
+  std::ostream& print(std::ostream& os) const override;
+};
+
+class OrderBlockade : public Order {
+public:
+  OrderBlockade();
+  OrderBlockade(const OrderBlockade& other);
+  bool validate() override;
+  void execute() override;
+  OrderBlockade& operator=(const OrderBlockade& other);
+  std::ostream& print(std::ostream& os) const override;
+};
+
+class OrderAirlift : public Order {
+public:
+  OrderAirlift();
+  OrderAirlift(const OrderAirlift& other);
+  bool validate() override;
+  void execute() override;
+  OrderAirlift& operator=(const OrderAirlift& other);
+  std::ostream& print(std::ostream& os) const override;
+};
+
+class OrderNegotiate : public Order {
+public:
+  OrderNegotiate();
+  OrderNegotiate(const OrderNegotiate& other);
+  bool validate() override;
+  void execute() override;
+  OrderNegotiate& operator=(const OrderNegotiate& other);
+  std::ostream& print(std::ostream& os) const override;
+};
+
+class OrderList {
+public:
+  std::vector<Order>* orders;
+
+  OrderList();
+  OrderList(const OrderList& other);
+  OrderList& operator=(const OrderList& other);
+  std::ostream& print(std::ostream& os) const;
+  void add(Order* order);
+  void remove(int index);
+  void move(int fromIndex, int toIndex);
+  ~OrderList();
+};
--- A1/src/core/orders/Orders.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMP_354_ORDERS_H
-#define COMP_354_ORDERS_H
-
-
-class Orders {
-};
-
-
-#endif //COMP_354_ORDERS_H
