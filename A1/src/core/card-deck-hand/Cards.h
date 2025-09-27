c0a9a973dc7a6aa139788f4a7910a665d26f6493
sanjitt-k <sanjittk@outlook.com>
2025-09-27 14:50:31 -0400

    Update Cards.h
    Implements Cards.h - header file for Part 4

--- A1/src/core/card-deck-hand/Cards.h
+++ A1/src/core/card-deck-hand/Cards.h
@@ -1,9 +1,68 @@
-#ifndef A1_CARDS_H
-#define A1_CARDS_H
+//
+// Part 4: Cards deck/hand
+//Cards.h
+//
 
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
-class Cards {
+// Forward declarations (real classes implemented in Part 2 and 3)
+class Player;
+class OrdersList;
+
+class Card {
+private:
+    std::string* type;  // pointer type (assignment rule)
+
+public:
+    Card(const std::string& t);
+    Card(const Card& other); // copy constructor
+    Card& operator=(const Card& other); // assignment operator
+    ~Card();
+
+    std::string getType() const;
+    void play(Player* player, OrdersList* ordersList); // creates order, returns card to deck
+
+    friend std::ostream& operator<<(std::ostream& out, const Card& c);
 };
 
+class Deck {
+private:
+    std::vector<Card*>* cards;  // collection of cards
 
-#endif //A1_CARDS_H
+public:
+    Deck();
+    Deck(const Deck& other);
+    Deck& operator=(const Deck& other);
+    ~Deck();
+
+    Card* draw();                  // remove random card, return it
+    void returnCard(Card* card);   // put card back into deck
+    void printDeck() const;
+
+    // ADD: Stream insertion operator
+    friend std::ostream& operator<<(std::ostream& out, const Deck& deck);
+};
+
+class Hand {
+private:
+    std::vector<Card*>* handCards;  // player's current cards
+
+public:
+    Hand();
+    Hand(const Hand& other);
+    Hand& operator=(const Hand& other);
+    ~Hand();
+
+    void addCard(Card* card);
+    //deck parameter to properly return cards
+    void playAll(Player* player, OrdersList* ordersList, Deck* deck);
+    void printHand() const;
+
+    // ADD: Stream insertion operator
+    friend std::ostream& operator<<(std::ostream& out, const Hand& hand);
+};
